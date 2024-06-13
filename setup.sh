8b08087a84aecedcb4c11dd6c3f4d1ada12d0fee
le thi hue <kimhuegg@gmail.com>
2024-06-13 22:51:55 +0700

    ok

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres@localhost:5432/postgres"
+export DATABASE_URL="postgres://u9ktkm73k3o8h8:pb04b32b7572aeb36338ce7f28200ad6bc98416ff2837355a7501b3a37d2abe49@c8lj070d5ubs83.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/db0jp24hdpkt09"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
